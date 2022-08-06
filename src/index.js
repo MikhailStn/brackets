098d34d826b9e73c49cbd475d46db1ea58728765
Mikhail Stankevich <mikhailstn1@gmail.com>
2022-08-07 01:09:26 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (str[i] === bracketsConfig[j][0] && str[i + 1] === bracketsConfig[j][1]) {
+        str = str.slice(0, i) + str.slice(i + 2, str.length)
+        i = i - 2
+      }
+    }
+  }
+  return str.length === 0
 }
